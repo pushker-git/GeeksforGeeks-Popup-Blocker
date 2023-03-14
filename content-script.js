e692e2aebc4d571a9d9ed33f9c06c42f9b0905c7
Avik Kundu <avikkundu8d@gmail.com>
2023-03-14 14:43:53 +0530

    Updated logic

--- content-script.js
+++ content-script.js
@@ -1,2 +1,9 @@
 // set the view count to less than 10 to avoid forcefully login
 localStorage.setItem("gfgViewCount", 1);
+
+//removing the popup modal from DOM
+const divs = document.querySelectorAll('.login-modal-div');
+
+divs.forEach(div => {
+  div.remove();
+});
