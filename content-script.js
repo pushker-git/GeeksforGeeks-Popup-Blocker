73c751fde46f743bd2a9287d0554a246722b0be3
Avik Kundu <1828008@kiit.ac.in>
2023-08-18 22:39:50 +0530

    Added timeout

--- content-script.js
+++ content-script.js
@@ -4,6 +4,8 @@ localStorage.setItem("gfgViewCount", 1);
 //removing the popup modal from DOM
 const divs = document.querySelectorAll('.login-modal-div');
 
+setTimeout(function() {
 divs.forEach(div => {
   div.remove();
 });
+}, 3000); // Timeout added if modal takes long to appear
